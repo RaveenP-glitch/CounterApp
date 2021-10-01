1fcb6224aad99822b34a3f2e01ea0f3d9cfee89f
Raveen Panditha <raveenpanditha9@gmail.com>
2021-10-01 19:42:13 +0530

    new lines
    new lines

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,16 +3,48 @@ import React, { Component } from 'react';
 
 class Counter extends React.Component {
     state={ 
-        count: 0
-        
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
+      
     };
 
+    // constructor(){
+    //     super();
+    //     this.handleIncrement =this.handleIncrement.bind(this);
+
+    // }
+
+    handleIncrement = (product) =>{
+        console.log(product);
+       this.setState({ count: this.state.count + 1 })
+
+    };
+
+ 
+
+    renderTags(){
+        if(this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+        return <ul>{ this.state.tags.map(tag => <li key={tag}>{ tag }</li>) }</ul>
+    }
+   
+
     render() { 
+      
         return <div>
-            <span>{this.formatCount()}</span>
-            <button>Increment</button>
+            {/* <img src={ this.state.imageUrl } alt=" " /> */}
+            <span style={ { fontSize: 20 } } className={ this.getBadgeClasses() }>{this.formatCount()}</span>
+            <button onClick={() => this.handleIncrement(product)} className="btn btn-secondary btn-sm">Increment</button>
+            { this.state.tags.length === 0 && 'Please create a new tag!' }
+            { this.renderTags() }
 
         </div>;
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 
     formatCount(){
