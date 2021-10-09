37bf3bf5e820d71ca6e9d919020da0407c4b1733
Raveen Panditha <raveenpanditha9@gmail.com>
2021-10-09 14:45:24 +0530

    new lines

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 
 class Counter extends React.Component {
     state={ 
-        count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
+        value: this.props.value
+        
       
     };
 
@@ -16,17 +16,17 @@ class Counter extends React.Component {
 
     handleIncrement = (product) =>{
       
-       this.setState({ count: this.state.count + 1 })
+       this.setState({ cvalue: this.state.count + 1 });
 
     };
 
  
 
-    renderTags(){
-        if(this.state.tags.length === 0) return <p>There are no tags!</p>;
+    // renderTags(){
+    //     if(this.state.tags.length === 0) return <p>There are no tags!</p>;
 
-        return <ul>{ this.state.tags.map(tag => <li key={tag}>{ tag }</li>) }</ul>
-    }
+    //     return <ul>{ this.state.tags.map(tag => <li key={tag}>{ tag }</li>) }</ul>
+    // }
    
 
     render() { 
@@ -35,8 +35,8 @@ class Counter extends React.Component {
             {/* <img src={ this.state.imageUrl } alt=" " /> */}
             <span style={ { fontSize: 20 } } className={ this.getBadgeClasses() }>{this.formatCount()}</span>
             <button onClick={ () => this.handleIncrement({ id: 1}) } className="btn btn-secondary btn-sm">Increment</button>
-            { this.state.tags.length === 0 && 'Please create a new tag!' }
-            { this.renderTags() }
+            {/* { this.state.tags.length === 0 && 'Please create a new tag!' }
+            { this.renderTags() } */}
 
         </div>;
     }
